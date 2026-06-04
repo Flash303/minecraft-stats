24c1135a3a9f6a0177b945ca9627ec56f2381e59
Flash303 <Flash303mc@gmail.com>
2026-06-04 22:10:00 +0200

    feat: update server infos

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -3,10 +3,11 @@ use repository::models::record::Record;
 use repository::repository::{PostgresRepository, Repository};
 use sqlx::postgres::PgPoolOptions;
 use std::env;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 use time::OffsetDateTime;
 use tokio::task::JoinHandle;
 use tokio::time::sleep;
+use repository::models::server::ServerStatus;
 
 #[tokio::main]
 async fn main() {
@@ -32,22 +33,34 @@ async fn main() {
     println!("Initialized successfully!");
 
     loop {
+        let count_time = Instant::now();
+        println!("Pinging...");
+
         let possible_servers = repository.list_servers().await;
         if let Ok(servers) = possible_servers {
             let mut tasks: Vec<JoinHandle<Option<Record>>> = Vec::new();
 
-            for server in servers {
+            for mut server in servers {
+                let task_repository = repository.clone();
                 let task = tokio::spawn(async move {
-                    for _i in 0..3 {
+                    for i in 0..3 {
                         let ping_rs = ping_server(server.ip.as_str(), server.port).await;
                         if let Ok(ping) = ping_rs {
+                            server.last_favicon = ping.favicon;
+                            server.last_status = Some(ServerStatus::Online);
+                            server.last_connected = Some(ping.players.online);
+                            task_repository.update_server(&server).await.unwrap();
+
                             return Some(Record {
                                 server_id: server.id,
                                 date: OffsetDateTime::now_utc(),
                                 value: ping.players.online,
                             })
-                        } else {
+                        } else if i == 2 {
                             println!("Error in ping the server {} : {:?}", server.name, ping_rs.err());
+                            server.last_status = Some(ServerStatus::Offline);
+                            server.last_connected = None;
+                            task_repository.update_server(&server).await.unwrap();
                         }
                     }
 
@@ -69,6 +82,8 @@ async fn main() {
         } else {
             println!("Failed to retrieve possible servers: {:?}", possible_servers.err());
         }
+
+        println!("Ping duration : {:?}ms", count_time.elapsed().as_millis());
 
         sleep(Duration::from_secs(10)).await;
     }
