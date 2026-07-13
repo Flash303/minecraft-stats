d730082fd44a19987fb497b1505ede1856eb9379
Flash <Flash303mc@gmail.com>
2026-07-14 01:34:27 +0200

    feat: better ip copy

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,18 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
 }
+
+export function getServerIp(ip: string, port: number) {
+    let displayIp: string = `${ip}`;
+    if (port !== 25565) {
+        displayIp = `${ip}:${port}`;
+    }
+
+    const fullIp = `${ip}:${port}`;
+    return { displayIp, fullIp };
+}
+
+export function copyServerIp(ip: string, port: number): Promise<void> {
+    const { displayIp } = getServerIp(ip, port);
+    return navigator.clipboard.writeText(displayIp);
+}
